456c0a1e3fc0149a7a234951db59d759d2e7668f
RMitsui <mitsui.r.aa@m.titech.ac.jp>
2020-05-08 17:27:39 +0900

    add greet

--- app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
@@ -2,11 +2,24 @@ package jp.ac.titech.itpro.sdl.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var outputView:TextView? = null
+    private var inputName:EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        outputView = findViewById(R.id.output_view)
+        inputName = findViewById(R.id.input_name)
+    }
+
+    fun greet(v:View){
+        var name= this.inputName?.text.toString().trim()
+        if(name.isNotEmpty()) outputView?.text = "Hello $name!\nNice to see you!"
     }
 }
